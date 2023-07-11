f3575195367c18056e3ff448af742f254eb69fb8
Aionius Dactlys <aionius@gmail.com>
2023-07-11 11:05:21 -0400

    implemented redux-persist and redux-devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,36 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
+import { loggerMiddleware } from './middleware/logger';
 
-  console.log('type', action.type);
-  console.log('payload', action.payload);
-  console.log('currentState', store.getState());
-
-  next(action);
-
-  console.log('next state: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
 };
 
-const middleWares = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
-// export const store = createStore(rootReducer);
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && loggerMiddleware,
+].filter(Boolean);
+
+const composedEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
